eb8ad73147a854591645af9bb14b64bb1831dd6e
raanym <raaneem@gmail.com>
2021-07-08 19:42:21 +0500

    form content styling and work

--- src/components/FormContent.js
+++ src/components/FormContent.js
@@ -1,7 +1,9 @@
-const FormContent = () => {
+import { ReactComponent as Thoondu } from '../img/thoondu.svg';
+
+const FormContent = (props) => {
 
   const formContent = [
-    { title: 'ތޫނޑު' },
+    { title: 'ތޫނޑު', image: <Thoondu className='w-20 h-20' /> },
     { title: 'ބަނދަރޮ ސަރަހައްދު' },
     { title: 'ބިޅިފޭށި' },
     { title: 'ރަސްމީ ބޯޅަދަނޑޮ' },
@@ -13,22 +15,30 @@ const FormContent = () => {
     { title: 'އެހެން ސަރަހައްދެއްވެސް ތަރައްޤީކުރަން ޚިޔާލެއްވާނަމަ ފޮނުވައިދެއްވާ' }
   ]
 
-  if (formContent[0])
+  const hoverHandler = (e) => {
 
-    return (
-      <div className='sm:px-52 px-4 sm:grid sm:grid-cols-3'>
-        {formContent.map((content, i) => {
-          if ((formContent.length - 1) === i) {
-            return <a href='' className='py-8 bg-blue-200 m-2 sm:col-span-2'>{content.title}</a>
-          } else if ((formContent.length - 2) === i) {
-            return <a href='' className='py-8 bg-blue-200 m-2 sm:row-span-2'>{content.title}</a>
-          } else {
-            return <a href='' className='py-8 bg-blue-200 m-2' > {content.title}</a>
-          }
+    if (!e.target.classList.contains('bg-blue-500')) {
+      e.target.classList.add('bg-blue-500');
+    } else {
+      e.target.classList.remove('bg-blue-500');
+    }
+  }
 
-        })}
-      </div >
-    )
+
+  return (
+    <div className='sm:px-52 px-2 sm:grid sm:grid-cols-3 block flex flex-col font-faseyha text-xl leading-loose'>
+      {formContent.map((content, i) => {
+        if ((formContent.length - 1) === i) {
+          return <a href='#' key={content.title} className='hover:bg-gray-300 py-8 px-4 bg-blue-300 m-2 sm:col-span-2'>{content.title}</a>
+        } else if ((formContent.length - 2) === i) {
+          return <a href='#' key={content.title} className='hover:bg-gray-400 py-8 px-4 bg-gray-300 m-2 sm:row-span-2 block'>{content.title}</a>
+        } else {
+          return <a href='#' key={content.title} className='flex justify-between py-8 px-4 bg-gray-300 m-2 hover:bg-gray-400' > {content.title} {content.image}</a>
+        }
+
+      })}
+    </div >
+  )
 }
 
 export default FormContent;
--- src/components/HeaderBottom.js
+++ src/components/HeaderBottom.js
@@ -3,11 +3,11 @@ const HeaderBottom = () => {
   return (
     <div className='font-faseyha border-t-2 border-gray-100 py-2 hidden sm:block bg-white text-sm'>
       <ul className='flex items-center justify-center'>
-        <li className='px-2'><a class='hover:bg-black hover:text-white py-2 px-2 h-full rounded-md' href=''>ކުރިއަށްހުރި ސެޝަންތައް</a></li>
-        <li className='px-2'><a href='' class='hover:bg-black hover:text-white py-2 px-2 h-full rounded-md'>މައްސަލަތަކާއި ރަނގަޅުކުރަންޖެހޭ ކަންތައް</a></li>
+        < li className='px-2' > <a className='hover:bg-black hover:text-white py-2 px-2 h-full rounded-md' href=''>ކުރިއަށްހުރި ސެޝަންތައް</a></li >
+        <li className='px-2'><a href='' className='hover:bg-black hover:text-white py-2 px-2 h-full rounded-md'>މައްސަލަތަކާއި ރަނގަޅުކުރަންޖެހޭ ކަންތައް</a></li>
         <li className='px-2'><a href='' className='hover:bg-black hover:text-white py-2 px-2 h-full rounded-md'>ސާރވޭ</a></li>
-      </ul>
-    </div>
+      </ul >
+    </div >
   )
 }
 
