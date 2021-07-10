5c5ed35708dfcba210b79b3277c4993748f87598
raanym <raaneem@gmail.com>
2021-07-10 22:29:37 +0500

    form content finalize

--- src/components/FormContent.js
+++ src/components/FormContent.js
@@ -1,39 +1,30 @@
-import { ReactComponent as Thoondu } from '../img/thoondu.svg';
+import './FormContent.css';
 
 const FormContent = (props) => {
 
   const formContent = [
-    { title: 'ތޫނޑު', image: <Thoondu className='w-20 h-20' /> },
-    { title: 'ބަނދަރޮ ސަރަހައްދު' },
-    { title: 'ބިޅިފޭށި' },
-    { title: 'ރަސްމީ ބޯޅަދަނޑޮ' },
-    { title: 'ދަޑިމަގި ކިޅިއާ ވީދަޑޮ' },
-    { title: 'ބަންޑާރަކިޅިއާ ވީދަޑޮ' },
-    { title: 'ސްޕޯޓްސް ޕާކް' },
-    { title: 'ދޫނޑިގަން ސްޕޯޓްސް އެރީނާ' },
-    { title: 'ދަޑިމަގި ސްޕޯޓްސް އެރީނާ' },
+    { title: 'ތޫނޑު', class: 'form-el bg-thundi' },
+    { title: 'ބަނދަރޮ ސަރަހައްދު', class: 'form-el bg-harbour' },
+    { title: 'ބިޅިފޭށި', class: 'form-el bg-bilhifeyshi' },
+    { title: 'ރަސްމީ ބޯޅަދަނޑޮ', class: 'form-el bg-rasmee-dhandu' },
+    { title: 'ދަޑިމަގި ކިޅިއާ ވީދަޑޮ', class: 'form-el bg-dkilhi' },
+    { title: 'ބަންޑާރަކިޅިއާ ވީދަޑޮ', class: 'form-el bg-bkilhi' },
+    { title: 'ސްޕޯޓްސް ޕާކް', class: 'form-el bg-scomplex' },
+    { title: 'ދޫނޑިގަން ސްޕޯޓްސް އެރީނާ', class: 'form-el bg-dhg-arena' },
+    { title: 'ދަޑިމަގި ސްޕޯޓްސް އެރީނާ', class: 'form-el bg-dha-arena' },
     { title: 'އެހެން ސަރަހައްދެއްވެސް ތަރައްޤީކުރަން ޚިޔާލެއްވާނަމަ ފޮނުވައިދެއްވާ' }
   ]
 
-  const hoverHandler = (e) => {
-
-    if (!e.target.classList.contains('bg-blue-500')) {
-      e.target.classList.add('bg-blue-500');
-    } else {
-      e.target.classList.remove('bg-blue-500');
-    }
-  }
-
 
   return (
-    <div className='sm:px-52 px-2 sm:grid sm:grid-cols-3 block flex flex-col font-faseyha text-xl leading-loose'>
+    <div className='sm:px-52 px-2 sm:grid sm:grid-cols-3 block flex flex-col font-waheed leading-loose'>
       {formContent.map((content, i) => {
         if ((formContent.length - 1) === i) {
-          return <a href='#' key={content.title} className='hover:bg-gray-300 py-8 px-4 bg-blue-300 m-2 sm:col-span-2'>{content.title}</a>
+          return <a href='#' key={content.title} className={`${content.class} bg-cover opacity-70 hover:opacity-100 text-2xl flex justify-center items-center py-8 px-4 bg-gray-300 m-1 col-span-2`}>{content.title}</a>
         } else if ((formContent.length - 2) === i) {
-          return <a href='#' key={content.title} className='hover:bg-gray-400 py-8 px-4 bg-gray-300 m-2 sm:row-span-2 block'>{content.title}</a>
+          return <a href='#' key={content.title} className={`${content.class} row-span-2 bg-cover hover:text-gray-200 opacity-70 hover:opacity-100 text-2xl flex justify-center items-center py-8 px-4 bg-gray-300 m-1 hover:bg-gray-400 hover:text-xl`}>{content.title}</a>
         } else {
-          return <a href='#' key={content.title} className='flex justify-between py-8 px-4 bg-gray-300 m-2 hover:bg-gray-400' > {content.title} {content.image}</a>
+          return <a href='#' key={content.title} className={`${content.class} bg-cover hover:text-teal transform hover:text-3xl opacity-70 hover:opacity-100 text-2xl flex justify-center items-center py-8 px-4 bg-gray-300 m-1 hover:bg-gray-700 h-40`} > {content.title}</a>
         }
 
       })}
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,17 @@ module.exports = {
         waheed: ['MV_Waheed'],
         faseyha: ['MV_Faseyha'],
       },
+      backgroundImage: {
+        'thundi': "url('img/thundi.jpg')",
+        'harbour': "url('img/harbour.jpg')",
+        'bilhifeyshi': "url('img/bilhifeyshi.jpg')",
+        'rasmee-dhandu': "url('img/rasmee-dhandu.jpg')",
+        'dkilhi': "url('img/dhadimagi-kilhi.jpg')",
+        'bkilhi': "url('img/Bandaara-Kilhi.jpg')",
+        'scomplex': "url('img/sports-arena.jpg')",
+        'dhg-arena': "url('img/dhoodigan-arena.jpg')",
+        'dha-arena': "url('img/dhadimago-arena.jpg')",
+      },
     },
   },
   variants: {
