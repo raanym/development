617ee405530f1a16a1ce4fa9dd0d041a7aeeef63
raanym <raaneem@gmail.com>
2021-07-15 13:56:00 +0500

    add link to home icon

--- src/components/HeaderTop.js
+++ src/components/HeaderTop.js
@@ -5,7 +5,7 @@ const HeaderTop = () => {
 
   return (
     <div className='flex items-center justify-center py-2'>
-      <a href='#'>
+      <a href='https://plan.fuvahmulah.gov.mv'>
         <img src={logo} className='w-20' />
       </a>
     </div>
