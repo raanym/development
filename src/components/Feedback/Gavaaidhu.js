27e3e3bd7ce764fb5c40616b676485fd8f1f9745
raanym <raaneem@gmail.com>
2021-10-14 15:58:07 +0500

    updates

--- src/components/Feedback/Gavaaidhu.js
+++ src/components/Feedback/Gavaaidhu.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const Gavaaidhu = (props) => {
 
   return (
-    <li className='border-b-2 border-gray-200 rounded-lg py-4 font-faseyha font-thin mt-8 text-thin'>
+    <li className='border-b-2 border-gray-200 rounded-lg sm:py-4 py-3 font-faseyha font-thin text-thin'>
       <h3 className='leading-relaxed text-lg mb-2 word-break text-gray-800'>{props.name}</h3>
       <a href={props.download} target='_blank' className='text-base text-blue-500 hover:text-blue-800'>ޑައުންލޯޑް</a>
       <Link className='tracking-tighter mr-6 text-blue-500 hover:text-blue-800 text-base' to={props.form}>ޚިޔާލު ފޮނުއްވާ</Link>
--- src/components/Feedback/OpenForFeedback.js
+++ src/components/Feedback/OpenForFeedback.js
@@ -18,26 +18,7 @@ const OpenForFeedback = () => {
         <GavaaidhuMain />
         <Other />
       </div >
-      <blockquote className="self-center row-span-2 relative shadow-lg p-4 text-xl italic border-l-4 bg-neutral-100 text-neutral-600 border-neutral-500 quote bg-white font-faseyha row-span-1 hidden sm:block">
-        <div className="stylistic-quote-mark" aria-hidden="true">
-          &ldquo;
-        </div>
-        <p className="mb-4 text-base text-gray-500 leading-relaxed">ކޮންމެ ކަމެއްވެސް ނިންމުމާއި ބައްޓަންކުރުމުގައި އެންމެ ފުރަތަމަ ހޯދާނީ ރައްޔިތުންގެ ލަފާ އަދި ޚިޔާލުތައް، މިކަންކަމައް ބެލީމަ އެނގެނީ ރައްޔިތުން ބޭނުންވާ ގޮތްތަކަކީ ކޮބައިކަން!</p>
-        <cite className="flex items-center">
-          <div className="flex flex-col items-start">
-            <span className="mb-1 text-teal text-base font-waheed">އިސްމާޢީލް ރަފީޤް</span>
-            <a
-              href="..."
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-sm"
-            >
-              ފުވައްމުލައް ސިޓީ މޭޔަރ
-            </a>
-          </div>
-        </cite>
-      </blockquote>
-      {/* <FeedbackIcon className='absolute md:-top-32 left-0 top-0 md:w-96 md:h-auto h-48 w-60 md:mt-20 opacity-90 sm:block hidden' /> */}
+      <FeedbackIcon className='md:w-96 md:h-auto h-48 w-60 md:mt-20 opacity-90 sm:block hidden row-span-3 self-center col-span-1' />
     </div >
   );
 }
