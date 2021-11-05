2141ae1d3d81d07c0d67c783d3d7491472556bd8
raanym <raaneem@gmail.com>
2021-11-05 17:04:19 +0500

    updates

--- src/components/Feedback/Feedback.js
+++ src/components/Feedback/Feedback.js
@@ -6,7 +6,7 @@ const Feedback = () => {
   return (
     <div className='pb-10 md:pb-20 bg-gray-100 sm:px-6 lg:w-4/5 mx-auto px-4 lg:px-0 pt-5'>
       <OpenForFeedback />
-      <ClosedFeedback />
+      {/* <ClosedFeedback /> */}
     </div>
   );
 }
